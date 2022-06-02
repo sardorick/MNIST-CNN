aee36c2c9fb845d542a595f4f2fdfddbd6a40ad6
sardorbekzokirov <45316437+sardorick@users.noreply.github.com>
2022-06-02 16:30:14 +0200

    Test

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
+import matplotlib.pyplot as plt
 import numpy as np
-print(np.arange(10))
+import torch
+def view_classify(img, ps):
+
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels(np.arange(10))
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+# images, labels = next(iter(testloader))
+# images.resize_(images.shape[0], 1, 784)
+# logits = model.forward(images[0,:])
+# ps = F.softmax(logits, dim=1)
+# view_classify(images[0].view(1, 28, 28), ps)
